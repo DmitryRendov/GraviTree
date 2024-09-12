42e633fb245c2c5f4f2984d370cae94b88648dac
Dmitry Rendov <dmitry_rendov@epam.com>
2024-09-12 16:55:19 +0200

    1.21.1 - Upgrade to support the latest MC release

--- src/me/ryanhamshire/GraviTree/GraviTree.java
+++ src/me/ryanhamshire/GraviTree/GraviTree.java
@@ -3,8 +3,6 @@ package me.ryanhamshire.GraviTree;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.logging.Logger;
 
@@ -348,7 +346,7 @@ public class GraviTree extends JavaPlugin implements Listener
     private boolean blockIsRootType(Block block)
     {
         Material type = block.getType();
-        if(type == Material.DIRT || type == Material.GRASS || type == Material.STONE ||
+        if(type == Material.DIRT || type == Material.GRASS_BLOCK || type == Material.STONE ||
                 type == Material.COBBLESTONE || ExtraTags.TERRACOTTA.isTagged(type) || type == Material.SAND ||
                 type == Material.PODZOL || type == Material.NETHERRACK || type == Material.WARPED_NYLIUM ||
                 type == Material.CRIMSON_NYLIUM || type == Material.MANGROVE_ROOTS)
@@ -396,7 +394,7 @@ public class GraviTree extends JavaPlugin implements Listener
     static boolean blockIsTreeAdjacent(Block block)
     {
         Material type = block.getType();
-        if(Tag.LEAVES.isTagged(type) || type == Material.AIR || type == Material.VINE || type == Material.COCOA || type == Material.TORCH || type == Material.SNOW || type == Material.GRASS || type == Material.DIRT || type == Material.STONE || type == Material.COBBLESTONE || type == Material.TALL_GRASS || type == Material.WEEPING_VINES || type == Material.TWISTING_VINES || type == Material.NETHERRACK || type == Material.CRIMSON_NYLIUM || type == Material.CRIMSON_ROOTS || type == Material.WARPED_NYLIUM || type == Material.WARPED_ROOTS || type == Material.NETHER_WART_BLOCK || type == Material.WARPED_WART_BLOCK || type == Material.SHROOMLIGHT)
+        if(Tag.LEAVES.isTagged(type) || type == Material.AIR || type == Material.VINE || type == Material.COCOA || type == Material.TORCH || type == Material.SNOW || type == Material.GRASS_BLOCK || type == Material.DIRT || type == Material.STONE || type == Material.COBBLESTONE || type == Material.TALL_GRASS || type == Material.WEEPING_VINES || type == Material.TWISTING_VINES || type == Material.NETHERRACK || type == Material.CRIMSON_NYLIUM || type == Material.CRIMSON_ROOTS || type == Material.WARPED_NYLIUM || type == Material.WARPED_ROOTS || type == Material.NETHER_WART_BLOCK || type == Material.WARPED_WART_BLOCK || type == Material.SHROOMLIGHT)
         {
             return true;
         }
